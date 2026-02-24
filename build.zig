da967859813be71000f422a6281426eff0a25bf0
master <nebulaorion@qq.com>
2026-02-24 22:38:47 +0800

    fix: 使用原始 build.zig 并更新 GitHub Actions 使用 Zig 0.9.1

--- build.zig
+++ build.zig
@@ -26,12 +26,12 @@ pub fn build(b: *std.build.Builder) void {
     _ = std.fs.realpathAlloc(b.allocator, windows_kit_bin_root) catch @panic("windows_kit_bin_root not found");
 
     const prefix = b.fmt("{s}_{s}_{s}", .{arch_tag, conf_tag, sign_tag});
-    const output_dir = b.fmt("{s}/{s}", .{b.install_path, prefix});
+    b.exe_dir = b.fmt("{s}/{s}", .{b.install_path, prefix});
 
-    debug.print("- out: {s}\n", .{output_dir});
+    debug.print("- out: {s}\n", .{b.exe_dir});
 
     const tmp_path = b.fmt("tmp/{s}", .{prefix});
-    std.fs.cwd().makePath(tmp_path) catch @panic("unable to create tmp directory");
+    b.makePath(tmp_path) catch @panic("unable to create tmp directory");
 
     b.installFile(b.fmt("external/{s}/{s}/WinDivert.dll", .{windivert_dir, arch_tag}), b.fmt("{s}/WinDivert.dll", .{prefix}));
     switch (arch) {
@@ -46,9 +46,9 @@ pub fn build(b: *std.build.Builder) void {
     const res_obj_path = b.fmt("{s}/clumsy_res.obj", .{tmp_path});
 
     const rc_exe = b.findProgram(&.{
-        "rc.exe",
+        "rc",
     }, &.{
-        std.fs.path.join(b.allocator, &[2][]const u8{windows_kit_bin_root, @tagName(arch)}) catch unreachable,
+        b.pathJoin(&.{windows_kit_bin_root, @tagName(arch)}),
     }) catch @panic("unable to find `rc.exe`, check your windows_kit_bin_root");
 
     const archFlag = switch (arch) {
@@ -68,70 +68,64 @@ pub fn build(b: *std.build.Builder) void {
         "etc/clumsy.rc",
     });
 
-    // Zig 0.11.0: addExecutable 需要指定源文件或使用 addCSourceFile 添加
-    const exe = b.addExecutable(.{
-        .name = "clumsy",
-        .root_source_file = null,
-    });
+    const exe = b.addExecutable("clumsy", null);
 
     switch (conf) {
         .Debug => {
-            exe.setOptimizeMode(.Debug);
+            exe.setBuildMode(.Debug);
             exe.subsystem = .Console;
         },
         .Release => {
-            exe.setOptimizeMode(.ReleaseSafe);
+            exe.setBuildMode(.ReleaseSafe);
             exe.subsystem = .Windows;
         },
         .Ship => {
-            exe.setOptimizeMode(.ReleaseFast);
+            exe.setBuildMode(.ReleaseFast);
             exe.subsystem = .Windows;
         },
     }
+    const triple  = switch (arch) {
+        .x64 => "x86_64-windows-gnu",
+        .x86 => "i386-windows-gnu",
+    };
 
-    // Zig 0.11.0: 使用标准目标查询
-    const target = b.standardTargetOptions(.{});
+    const target = CrossTarget.parse(.{
+        .arch_os_abi = triple,
+    }) catch unreachable;
     exe.setTarget(target);
 
     exe.step.dependOn(&cmd.step);
-    
-    // Zig 0.11.0: addObjectFile 需要 LazyPath
-    exe.addObjectFile(.{ .path = res_obj_path });
-    
-    // Zig 0.11.0: addCSourceFile 使用 .{ .file = ..., .flags = ... } 格式
-    exe.addCSourceFile(.{ .file = .{ .path = "src/bandwidth.c" }, .flags = &.{} });
-    exe.addCSourceFile(.{ .file = .{ .path = "src/divert.c" }, .flags = &.{} });
-    exe.addCSourceFile(.{ .file = .{ .path = "src/drop.c" }, .flags = &.{} });
-    exe.addCSourceFile(.{ .file = .{ .path = "src/duplicate.c" }, .flags = &.{} });
-    exe.addCSourceFile(.{ .file = .{ .path = "src/elevate.c" }, .flags = &.{} });
-    exe.addCSourceFile(.{ .file = .{ .path = "src/i18n.c" }, .flags = &.{} });
-    exe.addCSourceFile(.{ .file = .{ .path = "src/lag.c" }, .flags = &.{} });
-    exe.addCSourceFile(.{ .file = .{ .path = "src/main.c" }, .flags = &.{} });
-    exe.addCSourceFile(.{ .file = .{ .path = "src/ood.c" }, .flags = &.{} });
-    exe.addCSourceFile(.{ .file = .{ .path = "src/packet.c" }, .flags = &.{} });
-    exe.addCSourceFile(.{ .file = .{ .path = "src/reset.c" }, .flags = &.{} });
-    exe.addCSourceFile(.{ .file = .{ .path = "src/tamper.c" }, .flags = &.{} });
-    exe.addCSourceFile(.{ .file = .{ .path = "src/throttle.c" }, .flags = &.{} });
-    exe.addCSourceFile(.{ .file = .{ .path = "src/utils.c" }, .flags = &.{} });
+    exe.addObjectFile(res_obj_path);
+    exe.addCSourceFile("src/bandwidth.c", &.{""});
+    exe.addCSourceFile("src/divert.c", &.{""});
+    exe.addCSourceFile("src/drop.c", &.{""});
+    exe.addCSourceFile("src/duplicate.c", &.{""});
+    exe.addCSourceFile("src/elevate.c", &.{""});
+    exe.addCSourceFile("src/i18n.c", &.{""});
+    exe.addCSourceFile("src/lag.c", &.{""});
+    exe.addCSourceFile("src/main.c", &.{""});
+    exe.addCSourceFile("src/ood.c", &.{""});
+    exe.addCSourceFile("src/packet.c", &.{""});
+    exe.addCSourceFile("src/reset.c", &.{""});
+    exe.addCSourceFile("src/tamper.c", &.{""});
+    exe.addCSourceFile("src/throttle.c", &.{""});
+    exe.addCSourceFile("src/utils.c", &.{""});
 
     if (arch == .x86)
-        exe.addCSourceFile(.{ .file = .{ .path = "etc/chkstk.s" }, .flags = &.{} });
+        exe.addCSourceFile("etc/chkstk.s", &.{""});
 
-    // Zig 0.11.0: addIncludeDir 更名为 addIncludePath
-    exe.addIncludePath(.{ .path = b.fmt("external/{s}/include", .{windivert_dir}) });
+    exe.addIncludeDir(b.fmt("external/{s}/include", .{windivert_dir}));
 
     const iupLib = switch (arch) {
         .x64 => "external/iup-3.30_Win64_mingw6_lib",
         .x86 => "external/iup-3.30_Win32_mingw6_lib",
     };
 
-    exe.addIncludePath(.{ .path = std.fs.path.join(b.allocator, &[2][]const u8{iupLib, "include"}) catch unreachable });
-    exe.addCSourceFile(.{ .file = .{ .path = std.fs.path.join(b.allocator, &[2][]const u8{iupLib, "libiup.a"}) catch unreachable }, .flags = &.{} });
+    exe.addIncludeDir(b.pathJoin(&.{iupLib, "include"}));
+    exe.addCSourceFile(b.pathJoin(&.{iupLib, "libiup.a"}), &.{""});
 
     exe.linkLibC();
-    
-    // Zig 0.11.0: addLibPath 更名为 addLibraryPath
-    exe.addLibraryPath(.{ .path = b.fmt("external/{s}/{s}", .{windivert_dir, arch_tag}) });
+    exe.addLibPath(b.fmt("external/{s}/{s}", .{windivert_dir, arch_tag}));
     exe.linkSystemLibrary("WinDivert");
     exe.linkSystemLibrary("comctl32");
     exe.linkSystemLibrary("Winmm");
@@ -142,10 +136,10 @@ pub fn build(b: *std.build.Builder) void {
     exe.linkSystemLibrary("uuid");
     exe.linkSystemLibrary("ole32");
 
-    const exe_install_step = b.addInstallArtifact(exe, .{});  
+    const exe_install_step = b.addInstallArtifact(exe);  
     if (conf == .Ship)
     {
-        const remove_pdb_step = RemoveOutFile.create(b, "clumsy.pdb", prefix);
+        const remove_pdb_step = RemoveOutFile.create(b, "clumsy.pdb");
         remove_pdb_step.step.dependOn(&exe_install_step.step);
         b.getInstallStep().dependOn(&remove_pdb_step.step);
     }
@@ -164,25 +158,20 @@ pub const RemoveOutFile = struct {
     step: Step,
     builder: *Builder,
     rel_path: []const u8,
-    prefix: []const u8,
 
-    pub fn create(builder: *Builder, rel_path: []const u8, prefix: []const u8) *@This() {
+    pub fn create(builder: *Builder, rel_path: []const u8) *@This() {
         const self = builder.allocator.create(@This()) catch unreachable;
         self.* = . {
             .step = Step.init(.custom, builder.fmt("RemoveOutFile {s}", .{rel_path}), builder.allocator, make),
             .builder = builder,
             .rel_path = rel_path,
-            .prefix = prefix,
         };
         return self;
     }
 
     fn make(step: *Step) anyerror!void {
         const self = @fieldParentPtr(RemoveOutFile, "step", step);
-        // 文件安装到 prefix 子目录中，需要包含 prefix 路径
-        const out_path = std.fs.path.join(self.builder.allocator, &[3][]const u8{self.builder.install_path, self.prefix, self.rel_path}) catch unreachable;
-        std.fs.cwd().deleteFile(out_path) catch |err| {
-            if (err != error.FileNotFound) return err;
-        };
+        const out_dir = try std.fs.openDirAbsolute(self.builder.exe_dir, .{});
+        try out_dir.deleteFile(self.rel_path);
     }
 };
